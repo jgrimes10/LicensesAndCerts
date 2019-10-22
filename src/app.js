01fbc6be0207140d83614c1ec0bf66c5f4345bfa
Justin Grimes <justingrimes@Justins-MBP.relias.int>
2019-10-22 15:54:50 -0400

    Add more loading to app.js for getting wallet address and loading

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ App = {
 
     load: async () => {
         await App.loadWeb3();
+        await App.loadAccount();
+        await App.loadContracts();
     },
 
     // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
@@ -39,22 +41,65 @@ App = {
         }
     },
 
-    // setLoading: (boolean) => {
-    //     App.loading = boolean;
-    //     const loader = $('#loader');
-    //     const content = $('#content');
-    //     if (boolean) {
-    //         loader.show();
-    //         content.hide();
-    //     } else {
-    //         loader.hide();
-    //         content.show();
-    //     }
-    // }
+    loadAccount: async () => {
+        App.account = web3.eth.accounts[0];
+        console.log("here is the account");
+        console.log(App.account);
+    },
+
+    loadContracts: async () => {
+        // Create a JavaScript version of the smart contracts
+        const licenseAndCert = await $.getJSON('LicenseAndCert.json');
+        App.contracts.LicenseAndCert = TruffleContract(licenseAndCert);
+        App.contracts.LicenseAndCert.serProvider(App.web3Provider);
+
+        // Hydrate the smart contracts with values from the blockchain
+        App.licenseAndCert = await App.contracts.LicenseAndCert.deployed();
+    },
+
+    render: async () => {
+        // Prevent double render
+        if (App.loading) {
+            return;
+        }
+
+        // Update app loading state
+        App.setLoading(true);
+
+        // Render stuff
+
+        // Update loading state
+        App.setLoading(false);
+    },
+
+    setLoading: (boolean) => {
+        App.loading = boolean;
+        const loader = $('#loader');
+        const content = $('#content');
+        if (boolean) {
+            loader.show();
+            content.hide();
+        } else {
+            loader.hide();
+            content.show();
+        }
+    }
 }
 
 $(() => {
     $(window).load(() => {
         App.load();
     });
+});
+
+var $template = $(".template");
+var hash = 2;
+
+$(".btn-add-panel").on("click", function () {
+    var $newPanel = $template.clone();
+    $newPanel.find(".collapse").removeClass("in");
+    $newPanel.find(".accordion-toggle").attr("href", "#" + (++hash))
+             .text("Dynamic panel #" + hash);
+    $newPanel.find(".panel-collapse").attr("id", hash).addClass("collapse").removeClass("in");
+    $("#accordion").append($newPanel.fadeIn());
 });
