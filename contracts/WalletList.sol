2409c0e0d2eebb690a90a0bac3e4793f86bc82d0
William Coleman <wcoleman@reliaslearning.com>
2019-10-22 12:21:15 -0400

    walletList contract progress

--- contracts/WalletList.sol
+++ contracts/WalletList.sol
@@ -2,25 +2,51 @@ pragma solidity ^0.5.0;
 
 contract WalletList {
 
-    address[] public wallets;
+    address[] public walletList;
+
+    uint public walletCount = 0;
 
     struct Wallet {
         uint id;
         string firstName;
         string lastName;
-        string walletAddress;
+        address walletAddress;
         bool deleted;
     }
 
-    function saveWallet() {
+    mapping(uint => Wallet) public wallets;
 
+    event addWalletToList(
+        uint id,
+        string firstName,
+        string lastName,
+        address walletAddress,
+        bool deleted
+    );
+
+    function addWallet(string memory _firstName, string memory _lastName, address _walletAddressToAdd) public {
+    walletCount++;
+    wallets[walletCount] = Wallet(walletCount, _firstName, _lastName, _walletAddressToAdd, false);
+    emit addWalletToList(walletCount, _firstName, _lastName, _walletAddressToAdd, false);
     }
+
+    event removeWalletFromList(
+        uint id
+    );
+
+    event getWalletByAddress(
+        uint id,
+        string firstName,
+        string lastName,
+        address walletAddress,
+        bool deleted
+    );
 
     function removeWallet() {
 
     }
 
-    function getWallets() {
+    function getWallet() {
 
     }
 }
